c5c9c459e8713e51fbd2e700f05288654db02c69
Jonas <jonas@verosoft.no>
2023-01-29 20:49:28 +0100

    meeh

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,32 @@ use anathema::vm::*;
 use anathema::widgets::nodegen::WidgetGenerator;
 use anathema::widgets::*;
 
+fn context() -> Context {
+    let mut root_ctx = Context::new();
+    let values = (0..30_000)
+        .map(|i| format!("{i} - world and some extra text").into())
+        .collect::<Vec<Value>>();
+    let data = Value::List(values);
+    root_ctx.insert("data", data);
+    root_ctx.insert("counter", 0.into());
+    root_ctx
+}
+
 fn main() {
     let template = r#"
-        vstack
-            for item in {{ data }}
-                text [color: red] "{{ item }} hello "
-                    span [color: magenta] "- bleep bloop -"
+        // hstack
+            // vstack
+            //     text [color: blue] "START I have {{ counter }} bunnies in a hat"
+            //     text "TIME {{ time }}"
+            //     for item in {{ data }}
+            //         text [color: red] "{{ item }} hello {{ counter }}"
+            //             span [color: magenta] "- bleep bloop -"
+            vstack
+                text [color: blue] "START I have {{ counter }} bunnies in a hat"
+                text "TIME {{ time }}"
+                for item in {{ data }}
+                    text [color: red] "{{ item }} hello {{ counter }}"
+                        span [color: magenta] "- bleep bloop -"
     "#;
 
     let (inst, consts) = compiler::compile(template).unwrap();
@@ -20,18 +40,7 @@ fn main() {
     let vm = VirtualMachine::new(inst, consts);
     let templates = vm.exec().unwrap();
 
-    // eprintln!("{templates:#?}");
-    // return;
-
-    let mut root_ctx = Context::new();
-
     let mut counter = 0;
-
-    let values = (0..30_000)
-        .map(|i| format!("{i} - world and some extra text").into())
-        .collect::<Vec<Value>>();
-    let data = Value::List(values);
-    root_ctx.insert("data", data);
 
     let lookup = WidgetLookup::default();
 
@@ -42,13 +51,17 @@ fn main() {
 
     let constraints = Constraints::new(width as usize, height as usize);
 
+    let mut root_ctx = context();
+
     loop {
-        root_ctx.insert("number", counter.into());
+        root_ctx.insert("counter", counter.into());
 
-        let mut gen: WidgetGenerator = WidgetGenerator::new(&templates, &lookup);
+        let mut gen: WidgetGenerator = WidgetGenerator::new(&templates, &root_ctx, &lookup);
+
+        let now = Instant::now();
+
         let mut widgets = vec![];
-
-        while let Some(mut widget) = gen.next_widget_container(&root_ctx) {
+        while let Some(mut widget) = gen.next_widget_container() {
             widget.layout(constraints, &root_ctx, &lookup);
             widgets.push(widget);
         }
@@ -67,9 +80,12 @@ fn main() {
             widget.paint(ctx);
         }
 
+        root_ctx.insert("time", format!("{:?}", now.elapsed()).into());
         screen.render(&mut output);
 
         std::thread::sleep_ms(1000);
         screen.erase();
+
+        counter += 1;
     }
 }
