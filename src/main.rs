394cfc1ad60345bab0f3aba1aa75f72fa6b2356b
Togglebit <me@togglebit.io>
2023-04-01 19:38:03 +0200

    got the slice!

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,58 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
 use std::io::stdout;
 use std::time::Instant;
 
 use anathema::compiler;
 use anathema::render::*;
 use anathema::vm::*;
-use anathema::widgets::nodegen::WidgetGenerator;
+use anathema::widgets::nodegen::gen2::Gen;
 use anathema::widgets::*;
+use anathema::widgets::{Viewport, Text};
 
-fn context() -> Context {
-    let mut root_ctx = Context::new();
-    let values = (0..30_000)
-        .map(|i| format!("{i} - world and some extra text").into())
+fn context() -> HashMap<String, Value> {
+    let longstring = read_to_string("./longlongtext.txt").unwrap();
+    let mut root_ctx = HashMap::new();
+    // let values = (0..3_000_000)
+    let values = (0..7)
+        .map(|i| (Value::from(i), Value::from(format!("counter: {i} - world and some extra text"))).into())
         .collect::<Vec<Value>>();
     let data = Value::List(values);
-    root_ctx.insert("data", data);
-    root_ctx.insert("counter", 0.into());
+    let map = Value::Map(HashMap::from([
+        ("list".to_string(), data)
+    ]));
+
+    root_ctx.insert("data".to_string(), map);
+    root_ctx.insert("counter".into(), 0.into());
+    root_ctx.insert("lark".into(), false.into());
+    root_ctx.insert("other".into(), false.into());
+    root_ctx.insert("longstring".into(), longstring.into());
     root_ctx
 }
 
 fn main() {
-    let template = r#"
-        // hstack
+    let template = "
+    // vstack
+    //     // text 'layout time: {{ time }} | render time: {{ render-time }}'
+    //     hstack
+            viewport [item-offset: 1, offset: 123]
+                text 'this is the top of a viewport'
+                for val in {{ data }}
+                    text '{{ val }}'
+                text 'this is the bottom of a viewport'
             // vstack
-            //     text [color: blue] "START I have {{ counter }} bunnies in a hat"
-            //     text "TIME {{ time }}"
-            //     for item in {{ data }}
-            //         text [color: red] "{{ item }} hello {{ counter }}"
-            //             span [color: magenta] "- bleep bloop -"
-            vstack
-                text [color: blue] "START I have {{ counter }} bunnies in a hat"
-                text "TIME {{ time }}"
-                for item in {{ data }}
-                    text [color: red] "{{ item }} hello {{ counter }}"
-                        span [color: magenta] "- bleep bloop -"
-    "#;
+            //     for val in {{ data }}
+            //         text '{{ val }}'
+    ";
+
+    let template = "
+        viewport [source: {{ data.list }}, binding: 'bob']
+            text 'this is the top of a viewport'
+            for val in {{ bob }}
+                text 'hello {{ val }}'
+            text 'this is the bottom of a viewport'
+    ";
+
 
     let (inst, consts) = compiler::compile(template).unwrap();
 
@@ -53,25 +72,23 @@ fn main() {
 
     let mut root_ctx = context();
 
+    let mut widgets = vec![];
+
     loop {
-        root_ctx.insert("counter", counter.into());
+        let mut now = Instant::now();
+        root_ctx.insert("counter".to_string(), counter.into());
 
-        let mut gen: WidgetGenerator = WidgetGenerator::new(&templates, &root_ctx, &lookup);
-
-        let now = Instant::now();
-
-        let mut widgets = vec![];
-        while let Some(mut widget) = gen.next_widget_container() {
-            widget.layout(constraints, &root_ctx, &lookup);
+        let mut values = Values::new(&root_ctx);
+        let mut genny = Gen::new(&templates, &lookup);
+        while let Some(mut widget) = genny.gen(&mut values) {
+            let values = values.into_layout();
+            widget.layout(constraints, &values, &lookup);
             widgets.push(widget);
         }
 
-        for widget in &mut widgets {
-            for child in &widget.children.widgets {
-                let size = child.size;
-                let x = size;
-            }
+        // // diff(&mut prev, &mut widgets);
 
+        for widget in &mut widgets {
             widget.position(Pos::ZERO);
         }
 
@@ -80,12 +97,17 @@ fn main() {
             widget.paint(ctx);
         }
 
-        root_ctx.insert("time", format!("{:?}", now.elapsed()).into());
-        screen.render(&mut output);
+        root_ctx.insert("time".to_string(), format!("{:?}", now.elapsed()).into());
 
-        std::thread::sleep_ms(1000);
+        // prev = widgets.drain(..).collect();
+
+        screen.render(&mut output);
+        root_ctx.insert("render-time".to_string(), format!("{:?}", now.elapsed()).into());
+        widgets.clear();
+
         screen.erase();
 
         counter += 1;
+        std::thread::sleep_ms(100);
     }
 }
