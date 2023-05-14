f117dc27ebccb787684115b8fe0e6a74e0cebec6
Togglebit <me@togglebit.io>
2023-05-14 14:50:29 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,39 @@ use anathema::compiler;
 use anathema::render::*;
 use anathema::runtime::Runtime;
 use anathema::vm::*;
-use anathema::widgets::nodegen::{WidgetGen, Widgets};
+use anathema::widgets::template::Template;
 use anathema::widgets::view::{DefaultView, View};
-use anathema::widgets::{Border, Text, VStack, Viewport, *};
+use anathema::widgets::{Border, *};
 
 struct TheView {
-    ctx: HashMap<String, Value>,
-    templates: Vec<WidgetTemplate>,
+    ctx: DataContext,
+    templates: Vec<Template>,
 }
 
 impl TheView {
-    pub fn new(templates: Vec<WidgetTemplate>) -> Self {
-        let mut ctx = HashMap::new();
+    pub fn new(templates: Vec<Template>) -> Self {
+        let mut ctx = DataContext::default();
         let values = (0..2).map(|i| Value::from(i)).collect::<Vec<_>>();
         ctx.insert("data".into(), Value::List(values));
         ctx.insert("offset".into(), Value::from(-1i64));
-        ctx.insert("item-offset".into(), Value::from(2i64));
+        ctx.insert("item-offset".into(), Value::from(1i64));
         Self { templates, ctx }
     }
 }
 
 impl View for TheView {
     fn update(&mut self) {
-        let Value::List(list) = self.ctx.get_mut("data").unwrap() else { panic!() };
+        // let Value::List(list) = self.ctx.get_mut("data").unwrap() else { panic!() };
         // list.push("updated".into());
         // let offset = self.ctx.get("offset").unwrap().to_signed_int().unwrap();
         // self.ctx.insert("offset".into(), Value::from(offset + 1));
     }
 
-    fn ctx(&self) -> &HashMap<String, Value> {
+    fn ctx(&self) -> &DataContext {
         &self.ctx
     }
 
-    fn templates(&self) -> &[WidgetTemplate] {
+    fn templates(&self) -> &[Template] {
         &self.templates
     }
 }
