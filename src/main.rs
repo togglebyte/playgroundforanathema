d9468bcf8fc67ea492e826a6c24ecfc6bba2c32b
Togglebit <me@togglebit.io>
2023-06-26 10:19:56 +0200

    lappy test

--- src/main.rs
+++ src/main.rs
@@ -8,58 +8,30 @@ use anathema::render::*;
 use anathema::runtime::Runtime;
 use anathema::vm::*;
 use anathema::widgets::template::Template;
-use anathema::widgets::view::{DefaultView, View};
-use anathema::widgets::{Border, *};
+use anathema::widgets::{Border, HStack, Spacer, VStack, Viewport, *};
 
-struct TheView {
-    ctx: DataContext,
-    templates: Vec<Template>,
-}
+fn ctx() -> DataCtx {
+    let mut ctx = DataCtx::default();
+    let values = (0..40_000).map(|i| Value::from(i)).collect::<Vec<_>>();
+    ctx.insert("data".to_string(), Value::List(values));
 
-impl TheView {
-    pub fn new(templates: Vec<Template>) -> Self {
-        let mut ctx = DataContext::default();
-        let values = (0..2).map(|i| Value::from(i)).collect::<Vec<_>>();
-        ctx.insert("data".into(), Value::List(values));
-        ctx.insert("offset".into(), Value::from(-1i64));
-        ctx.insert("item-offset".into(), Value::from(1i64));
-        Self { templates, ctx }
-    }
-}
-
-impl View for TheView {
-    fn update(&mut self) {
-        // let Value::List(list) = self.ctx.get_mut("data").unwrap() else { panic!() };
-        // list.push("updated".into());
-        // let offset = self.ctx.get("offset").unwrap().to_signed_int().unwrap();
-        // self.ctx.insert("offset".into(), Value::from(offset + 1));
-    }
-
-    fn ctx(&self) -> &DataContext {
-        &self.ctx
-    }
-
-    fn templates(&self) -> &[Template] {
-        &self.templates
-    }
+    let values = (0..40_000).map(|i| Value::from(i)).collect::<Vec<_>>();
+    ctx.insert("cols".to_string(), Value::List(values));
+    ctx
 }
 
 fn main() {
     let template = "
-        viewport [direction: forward, item: {{ item-offset }}, offset: {{ offset }}, source: {{ data }}, binding: x]
-            text 'first'
-            text 'second'
-            item
-                border
-                    text 'number: {{ x }}'
-            text 'end'
+    vstack
+        hstack
+            for _ in {{ cols }}
+                vstack
+                    for val in {{ data }}
+                        text 'X'
     ";
 
     let templates = templates(template).unwrap();
 
-    let view = TheView::new(templates);
-
-    let mut runtime = Runtime::new().unwrap();
-    runtime.load_view(Box::new(view));
+    let mut runtime = Runtime::new(&templates, ctx()).unwrap();
     runtime.run().unwrap();
 }
