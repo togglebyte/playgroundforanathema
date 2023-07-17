e891a002cc971d2565c9bbe91688ec8b8cb9a3b4
Togglebit <me@togglebit.io>
2023-07-17 23:34:37 +0200

    everything is perfect

--- src/main.rs
+++ src/main.rs
@@ -24,36 +24,36 @@ fn ctx() -> DataCtx {
     let mut ctx = DataCtx::default();
 
     let values = (0..40_000).map(|i| Value::from(i)).collect::<Vec<_>>();
-    ctx.insert("data".to_string(), Value::List(values));
+    ctx.insert("data".to_string(), values);
 
-    let values = (0..40_000).map(|i| Value::from(i)).collect::<Vec<_>>();
-    ctx.insert("cols".to_string(), Value::List(values));
+    let values = (0..6).map(|i| Value::from(i)).collect::<Vec<_>>();
+    ctx.insert("cols".to_string(), values);
 
-    let words = vec!["abra".to_string().into(), "crabdabra".to_string().into()];
-    ctx.insert("words".to_string(), Value::List(words));
+    let words = vec!["abra", "crabdabra"];
+    ctx.insert("words", words);
 
     ctx
 }
 
 fn main() {
+    tinylog::init_logger(true).unwrap();
+
     register_default_widgets();
     let main_templates = std::fs::read_to_string("templates/template.tiny").unwrap();
     let stats_templates = std::fs::read_to_string("templates/stats.tiny").unwrap();
 
     let main_templates = templates(&main_templates).unwrap();
     let stats_templates = templates(&stats_templates).unwrap();
-    let stats_view = StatsView { templates: stats_templates };
 
     let mut runtime = Runtime::new(
         main_templates,
         ctx(),
-        DefaultEvents(|ev, ctx, _| {
-            match ctx.get_mut::<i64>("counter") {
-                Some(counter) => *counter += 1,
-                None => ctx.insert("counter", 1),
-            }
+        DefaultEvents(|ev, ctx, nodes| {
+            *ctx.get_mut_or::<i64>("count_things", 0) += 1;
 
-            if let Event::KeyPress(KeyCode::Char('c'), KeyModifiers::CONTROL, ..) = ev {
+            if let Event::CtrlC = ev {
+                use std::fs::write;
+                write("/tmp/a", format!("{:#?}", nodes));
                 return Event::Quit;
             }
 
@@ -62,11 +62,8 @@ fn main() {
         DefaultEventProvider::with_timeout(200),
     )
     .unwrap();
-    runtime.register_view("stats", stats_view);
+
+    runtime.register_view("stats", StatsView { templates: stats_templates });
     runtime.enable_meta = true;
     runtime.run().unwrap();
 }
-
-// Login screen
-// Chat screen
-// Settings screen
