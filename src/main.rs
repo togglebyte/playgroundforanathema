27ffb923cedcc27d3f61280e0f0fdfbb5435ecbb
Togglebit <me@togglebit.io>
2023-10-24 17:12:01 +0200

    lalala

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,33 @@
 use std::borrow::Cow;
-use std::ops::{DerefMut, Deref};
+use std::ops::{Deref, DerefMut};
 
-use anathema::values::{State, Path, NodeId, Collection, Value, List};
+use anathema::core::Nodes;
 use anathema::runtime::events::{
     DefaultEventProvider, DefaultEvents, Event, KeyCode, KeyModifiers,
 };
 use anathema::runtime::Runtime;
+use anathema::values::{Collection, List, NodeId, Path, State, StateValue, ValueRef};
 use anathema::vm::*;
 use anathema::widgets::{Text, VStack};
 
-// fn ctx() -> DataCtx {
-//     let mut ctx = DataCtx::default();
-
-//     let values = (0..60_000).map(|i| Value::from(i)).collect::<Vec<_>>();
-//     ctx.insert("data".to_string(), values);
-
-//     let values = (0..12).map(|i| Value::from(i)).collect::<Vec<_>>();
-//     ctx.insert("cols".to_string(), values);
-
-//     let words = vec!["abra", "crabdabra"];
-//     ctx.insert("words", words);
-
-//     ctx
-// }
-
-
 struct MyState {
-    name: Value<String>,
-    counter: Value<usize>,
-    data: List<usize>
+    name: StateValue<String>,
+    counter: StateValue<usize>,
+    data: List<usize>,
 }
 
 impl MyState {
     pub fn new() -> Self {
         Self {
-            name: Value::new("Fin McSilverpants".to_string()),
-            counter: Value::new(0),
-            data: (0..60_000).collect::<Vec<_>>().into()
+            name: StateValue::new("Fiddlestick".to_string()),
+            counter: StateValue::new(0),
+            data: List::new((0..5)),
         }
     }
 }
 
 impl State for MyState {
-    fn get(&self, key: &Path, node_id: Option<&NodeId>) -> Option<Cow<'_, str>> {
+    fn get(&self, key: &Path, node_id: Option<&NodeId>) -> Option<ValueRef<'_>> {
         match key {
             Path::Key(s) if s == "name" => {
                 if let Some(node_id) = node_id.cloned() {
@@ -57,9 +42,12 @@ impl State for MyState {
                 Some((&self.counter).into())
             }
             Path::Composite(left, right) => {
-                let Path::Key(key) = left.deref() else { return None };
+                let Path::Key(key) = left.deref() else {
+                    return None;
+                };
                 if key == "data" {
-                    self.data.lookup(right)
+                    panic!()
+                    // self.data.lookup(right, node_id)
                 } else {
                     None
                 }
@@ -68,12 +56,27 @@ impl State for MyState {
         }
     }
 
-    fn get_collection(&self, key: &Path) -> Option<Collection> {
-        match key { 
-            Path::Key(s) if s == "data" => Some(Collection::State { path: key.clone(), len: self.data.len() }),
-            _ => None,
-        }
-    }
+    //    fn get_collection(&self, key: &Path, node_id: Option<&NodeId>) -> Option<Collection> {
+    //        match key {
+    //            Path::Key(s) => {
+    //                //    //
+    //                match s.as_str() {
+    //                    "data" => {
+    //                        //
+    //                        if let Some(node_id) = node_id.cloned() {
+    //                            self.data.subscribe(node_id);
+    //                        }
+    //                        Some(Collection::State {
+    //                            path: key.clone(),
+    //                            len: self.data.len(),
+    //                        })
+    //                    }
+    //                    _ => None,
+    //                }
+    //            }
+    //            _ => None,
+    //        }
+    //    }
 }
 
 fn main() {
@@ -84,26 +87,37 @@ fn main() {
     let main_expr = templates(&main_templates).unwrap();
 
     let mut runtime = Runtime::new(
-        main_expr,
+        &main_expr,
         MyState::new(),
-        DefaultEvents::<_, MyState>(|ev, nodes, state, meta| {
-            *state.counter = *meta.count;
+        DefaultEvents::<_, MyState>(
+            |ev, nodes, state| {
+                // *state.counter = *meta.count;
 
-            // let val: &mut usize = state.counter.deref_mut();
-            // state.counter += 1;
-            // *ctx.get_mut_or::<i64>("count_things", 0) += 1;
+                if let Event::KeyPress(KeyCode::Char(' '), ..) = ev {
+                    state.data.pop();
+                }
 
-            if let Event::MouseMove(x, y, ..) = ev {
-                // *ctx.get_mut_or::<i64>("x", 0i64) = x as i64;
-                // *ctx.get_mut_or::<i64>("y", 0i64) = y as i64;
-            }
+                if let Event::KeyPress(KeyCode::Char('='), ..) = ev {
+                    state.data.push(state.data.len());
+                }
 
-            if let Event::CtrlC = ev {
-                return Event::Quit;
-            }
+                // let val: &mut usize = state.counter.deref_mut();
+                // state.counter += 1;
+                // *ctx.get_mut_or::<i64>("count_things", 0) += 1;
 
-            ev
-        }, Default::default()),
+                if let Event::MouseMove(x, y, ..) = ev {
+                    // *ctx.get_mut_or::<i64>("x", 0i64) = x as i64;
+                    // *ctx.get_mut_or::<i64>("y", 0i64) = y as i64;
+                }
+
+                if let Event::CtrlC = ev {
+                    return Event::Quit;
+                }
+
+                ev
+            },
+            Default::default(),
+        ),
         DefaultEventProvider::with_timeout(100),
     )
     .unwrap();
