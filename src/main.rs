367f82197d537edb7b7481321537aceaadef7d2c
Togglebit <me@togglebit.io>
2023-11-16 16:22:08 +0100

    it's not working

--- src/main.rs
+++ src/main.rs
@@ -6,123 +6,45 @@ use anathema::runtime::events::{
     DefaultEventProvider, DefaultEvents, Event, KeyCode, KeyModifiers,
 };
 use anathema::runtime::Runtime;
-use anathema::values::{Collection, List, NodeId, Path, State, StateValue, ValueRef};
+use anathema::values::{Collection, List, NodeId, Path, State, StateValue, ValueRef, Map};
 use anathema::vm::*;
 use anathema::widgets::{Alignment, Border, Text, VStack};
 
+#[derive(Debug, State)]
 struct MyState {
     name: StateValue<String>,
     counter: StateValue<usize>,
     data: List<usize>,
-    nested_data: List<List<usize>>,
+    names: List<String>,
+    nested_data: List<List<List<usize>>>,
+    mappy: Map<Map<usize>>,
+    thing: Map<List<Map<&'static str>>>,
+    // things: List<Map<usize>>
 }
 
 impl MyState {
     pub fn new() -> Self {
+        let map = [("aaa", 1), ("bbb", 4)];
+        let map = [("aaa", Map::new(map))];
+        let map_2 = [("entry", List::new(vec![
+                Map::new([("lol", "omg I can't believe it I've never been this far away from")]),
+        ]))];
+        // let map_3 = [
+
         Self {
-            name: StateValue::new("Fiddlestick".to_string()),
+            name: StateValue::new("Fiddlesticks".to_string()),
             counter: StateValue::new(2),
-            data: List::new(0..2),
-            nested_data: List::new(vec![
-                List::new(0..2),
-                List::new(100..102),
-            ]),
+            data: List::new(0..4),
+            names: List::new(vec!["flip".to_string(), "flop".into()]),
+            nested_data: List::new(vec![List::new(vec![List::new(0..4), List::new(100..102)])]),
+            mappy: Map::new(map),
+            thing: Map::new(map_2),
+            // things: List::new(vec![
+            //     Map::new(map),
+            //     Map::new(map),
+            // ])
         }
     }
-}
-
-impl State for MyState {
-    fn get(&self, key: &Path, node_id: Option<&NodeId>) -> Option<ValueRef<'_>> {
-        match key {
-            Path::Key(s) if s == "name" => {
-                if let Some(node_id) = node_id.cloned() {
-                    self.name.subscribe(node_id);
-                }
-                Some((&self.name).into())
-            }
-            Path::Key(s) if s == "counter" => {
-                if let Some(node_id) = node_id.cloned() {
-                    self.counter.subscribe(node_id);
-                }
-                Some((&self.counter).into())
-            }
-            Path::Composite(left, right) => {
-                let Path::Key(key) = left.deref() else {
-                    return None;
-                };
-                if key == "data" {
-                    self.data.lookup(right, node_id).map(Into::into)
-                } else {
-                    None
-                }
-            }
-            // Path::Composite(left, right) => {
-            //     let Path::Key(key) = left.deref() else {
-            //         return None;
-            //     };
-            //     if key == "nested_data" {
-            //         self.nested_data.lookup(right, node_id).map(Into::into)
-            //     } else {
-            //         None
-            //     }
-            // }
-            _ => None,
-        }
-    }
-
-    fn get_collection(&self, key: &Path, node_id: Option<&NodeId>) -> Option<usize> {
-        match key {
-            Path::Key(s) if s == "data" => {
-                if let Some(node_id) = node_id.cloned() {
-                    self.data.subscribe(node_id);
-                }
-                Some(self.data.len())
-            }
-            Path::Key(s) if s == "nested_data" => {
-                if let Some(node_id) = node_id.cloned() {
-                    self.nested_data.subscribe(node_id);
-                }
-                Some(self.nested_data.len())
-            }
-            Path::Composite(lhs, rhs) {
-
-                Rethink how collections (and maps) are dealing with lookups.
-                They should be able to handle nested lookups:
-                e.g List<List<?>> / List<Map<?, List<??>>> / Map<?, Map<?, ??>>
-
-
-
-                if s == "nested_data" => {
-                if let Some(node_id) = node_id.cloned() {
-                    self.nested_data.subscribe(node_id);
-                }
-                Some(self.nested_data.len())
-            }
-            _ => None,
-        }
-    }
-
-    //    fn get_collection(&self, key: &Path, node_id: Option<&NodeId>) -> Option<Collection> {
-    //        match key {
-    //            Path::Key(s) => {
-    //                //    //
-    //                match s.as_str() {
-    //                    "data" => {
-    //                        //
-    //                        if let Some(node_id) = node_id.cloned() {
-    //                            self.data.subscribe(node_id);
-    //                        }
-    //                        Some(Collection::State {
-    //                            path: key.clone(),
-    //                            len: self.data.len(),
-    //                        })
-    //                    }
-    //                    _ => None,
-    //                }
-    //            }
-    //            _ => None,
-    //        }
-    //    }
 }
 
 fn main() {
@@ -142,7 +64,10 @@ fn main() {
                 }
 
                 if let Event::KeyPress(KeyCode::Char('d'), ..) = ev {
-                    state.data.remove(*state.counter);
+                    state.data.insert(0, 555);
+                    let s = format!("{:?}", state.data);
+
+                    let y = s;
                 }
                 // *state.counter = *meta.count;
 
@@ -151,7 +76,7 @@ fn main() {
                 }
 
                 if let Event::KeyPress(KeyCode::Char('='), ..) = ev {
-                    state.data.push(state.data.len());
+                    // state.data.push(state.data.len());
                 }
 
                 if let Event::KeyPress(KeyCode::Char('1'), ..) = ev {}
