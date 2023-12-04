f21dda48946e9a379ed44e68fd0181ab1d955aa6
Togglebit <me@togglebit.io>
2023-12-04 21:14:32 +0100

    more mess

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::borrow::Cow;
 use std::ops::{Deref, DerefMut};
 
+use anathema::core::views::View;
 use anathema::core::{Nodes, Display, Axis, Align};
 use anathema::runtime::events::{
     DefaultEventProvider, DefaultEvents, Event, KeyCode, KeyModifiers,
@@ -8,11 +9,34 @@ use anathema::runtime::events::{
 use anathema::runtime::Runtime;
 use anathema::values::{Collection, List, NodeId, Path, State, StateValue, ValueRef, Map};
 use anathema::vm::*;
-use anathema::widgets::{Alignment, Border, Text, VStack, ZStack, Position};
+// use anathema::widgets::{Alignment, Border, Text, VStack, ZStack, Position};
+use anathema::widgets::{Text};
+
+struct Items;
+
+impl View for Items {
+    type State = ();
+
+    fn event(&self, event: (), state: &mut Self::State) {
+        todo!()
+    }
+
+    fn make() -> Self {
+        Self
+    }
+}
 
 fn main() {
-    // let main_templates = std::fs::read_to_string("templates/app/main.tiny").unwrap();
-    let main_templates = std::fs::read_to_string("templates/template.tiny").unwrap();
+    let main_templates = std::fs::read_to_string("templates/app/main.tiny").unwrap();
+
+    Views has to be compiled first, so they exists in some kind of larger structure.
+    * Share consts maybe? (get rid of owned strings for views)
+    * What about hot reloading templates? Maybe a "large" reload function that
+      reloads everything?
+    * Let's add a `Template` struct that handles all the templates (can include hot-reloading too)
+
+
+    // let main_templates = std::fs::read_to_string("templates/template.tiny").unwrap();
 
     let main_expr = templates(&main_templates).unwrap();
 
@@ -40,6 +64,8 @@ fn main() {
         DefaultEventProvider::with_timeout(100),
     )
     .unwrap();
+
+    runtime.register_view("items", || Items);
 
     // runtime.register_view(
     //     "stats",
